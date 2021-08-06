9fa462a9dfb0a60058f74b555b164bd5165bd5c8
Percy Lam <lam_percy@hotmail.com>
2021-08-05 21:45:01 -0400

    update

--- samples/tabnet/tabnet.py
+++ samples/tabnet/tabnet.py
@@ -47,6 +47,8 @@ config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 
+devices = session.list_devices()
+print(devices)
 ############################################################
 #  Configurations
 ############################################################

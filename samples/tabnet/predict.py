fc78b432fe91e65b19cb3c0e44a704c870a947cd
Percy Lam <lam_percy@hotmail.com>
2021-08-07 19:04:47 -0400

    support directory predict

--- samples/tabnet/predict.py
+++ samples/tabnet/predict.py
@@ -15,34 +15,14 @@ session = tf.Session(config=config)
 
 class TabNetConfig(Config):
     NAME = "tab"
-
     IMAGES_PER_GPU = 1
-
     GPU_COUNT = 1
     NUM_CLASSES = 1 + 1  # COCO has 80 classes
-    DETECTION_MIN_CONFIDENCE = 0
+    DETECTION_MIN_CONFIDENCE = 0.75
 
 
-if __name__ == '__main__':
-    import argparse
-
-    parser = argparse.ArgumentParser(description='Train Mask R-CNN on Tables.')
-    parser.add_argument('--image_path',
-                        required=True)
-    parser.add_argument('--model_dir',
-                        required=True)
-    args = parser.parse_args()
-
-    config = TabNetConfig()
-
-    model = modellib.MaskRCNN(mode="inference",
-                              config=config,
-                              model_dir=args.model_dir)
-
-    model_path = model.find_last()
-    model.load_weights(model_path, by_name=True)
-
-    image = skimage.io.imread(args.image_path)
+def predict_once(image_path, model):
+    image = skimage.io.imread(image_path)
     if image.ndim != 3:
         image = skimage.color.gray2rgb(image)
         print('gray scale')
@@ -66,3 +46,30 @@ if __name__ == '__main__':
 
     result_path = name + '-predicted' + ext
     output_im.figure.savefig(result_path)
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Train Mask R-CNN on Tables.')
+    parser.add_argument('--image_path',
+                        required=True)
+    parser.add_argument('--model_dir',
+                        required=True)
+    args = parser.parse_args()
+
+    config = TabNetConfig()
+    model = modellib.MaskRCNN(mode="inference",
+                              config=config,
+                              model_dir=args.model_dir)
+    model_path = model.find_last()
+    model.load_weights(model_path, by_name=True)
+
+    if os.path.isdir(args.image_path):
+        for root, dirs, files in os.walk(args.image_path):
+            for f in files:
+                if os.path.splitext(f)[1] in ['.jpg', '.jpeg', '.png']:
+                    predict_once(os.path.join(root, f), model)
+
+    else:
+        predict_once(args.image_path, model)
